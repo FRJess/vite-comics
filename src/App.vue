6a47aa81f47c8c7e2c63aa8aaf11b5c54b3b0a3b
FR_Jess <fr_jess@icloud.com>
2022-11-15 15:33:42 +0100

    [add] all components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <script>
+
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooterTop from './components/AppFooterTop.vue';
+import AppFooterBottom from './components/AppFooterBottom.vue';
+
 export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooterTop,
+    AppFooterBottom,
+  },
 
 }
 </script>
 
 <template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooterTop/>
+  <AppFooterBottom/>
   
 </template>
 
 <style lang="scss">
+  @use 'styles/general';
 
 </style>
+
+
+
+<!-- **Descrizione:**
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
+**Bonus:**
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone. -->
