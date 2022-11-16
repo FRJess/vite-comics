4a051185ae89fa7d3f96efc5ff4ca42c006d862a
FR_Jess <fr_jess@icloud.com>
2022-11-16 16:45:57 +0100

    [add] hover on card

--- src/components/ComicCard.vue
+++ src/components/ComicCard.vue
@@ -12,6 +12,10 @@ export default {
   <div class="card">
     <img :src="card.thumb" alt="card.series">
     <h4>{{card.series}}</h4>
+    <p>
+      {{ card.price }} <br />
+      buy!
+    </p>
   </div>
   
 </template>
@@ -40,24 +44,24 @@ export default {
     display: inline-block;
     text-transform: uppercase;
   }
-  // p {
-  //   text-align: center;
-  //   position: absolute;
-  //   top: 30%;
-  //   left: 25%;
-  //   opacity: 0;
-  //   transition: 0.4s all;
-  //   color: white;
-  //   -webkit-text-stroke: 0.5px $secondary-color;
-  //   font-weight: bold;
-  //   font-size: 0.9rem;
-  // }
-  // &:hover p {
-  //   opacity: 1;
-  // }
-  // &:hover img {
-  //   filter: brightness(0.3);
-  // }
+  p {
+    text-align: center;
+    position: absolute;
+    top: 30%;
+    left: 25%;
+    opacity: 0;
+    transition: 0.4s all;
+    color: white;
+    text-transform: uppercase;
+    font-weight: 600;
+    font-size: 0.9rem;
+  }
+  &:hover p {
+    opacity: 1;
+  }
+  &:hover img {
+    filter: brightness(0.4);
+  }
 }
 
 </style>
