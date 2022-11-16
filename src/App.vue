d7e7fb98497ee58850f4bd1e409972b87313c660
FR_Jess <fr_jess@icloud.com>
2022-11-16 15:35:01 +0100

    [create] ComicCard file

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,8 @@ import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 import AppFooterTop from './components/AppFooterTop.vue';
 import AppFooterBottom from './components/AppFooterBottom.vue';
-import AppBlueMenu from "./components/AppBlueMenu.vue";
+import AppBlueMenu from './components/AppBlueMenu.vue';
+import ComicCard from './components/ComicCard.vue';
 
 export default {
   name: 'App',
@@ -14,6 +15,7 @@ export default {
     AppBlueMenu,
     AppFooterTop,
     AppFooterBottom,
+    ComicCard,
   },
 
 }
@@ -25,6 +27,7 @@ export default {
   <AppBlueMenu />
   <AppFooterTop/>
   <AppFooterBottom/>
+  <ComicCard/>
   
 </template>
 
@@ -39,4 +42,8 @@ export default {
 Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
 Quando la struttura a macroblocchi è pronta, popolate le voci di menu dinamicamente usando i data del componente.
 **Bonus:**
-Creare un componente aggiuntivo per gestire la fascia azzurra con le icone. -->
+Creare un componente aggiuntivo per gestire la fascia azzurra con le icone.
+
+Continuate a lavorare nella stessa repo di ieri e create un nuovo componente per visualizzare le card dei fumetti, prendendo i dati dal file json che trovate in allegato.
+Una volta inseriti tutti i contenuti dinamicamente, completate il vostro layout e rifinite i dettagli con Sass.
+In base a come vi è più comodo stabilite dove mettere l’array dei dati per poi passere le props alle card -->
---
+++ src/components/ComicCard.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "ComicCard",
+
+}
+</script>
+
+<template>
+  
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
