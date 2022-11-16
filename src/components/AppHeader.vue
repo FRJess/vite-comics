85e4ff2f501750ef0f5a9d9b469710d875c6e1b5
FR_Jess <fr_jess@icloud.com>
2022-11-16 22:12:17 +0100

    [improve] Header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,61 +1,11 @@
 <script>
+import menu from '../data/menu';
+
 export default {
   name: 'AppHeader',
   data(){
-    return{
-      menu:[
-        {
-          text: 'Characters',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'Comics',
-          href: '#',
-          current: true,
-        },
-        {
-          text: 'Movies',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'TV',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'Games',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'Collectibles',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'Videos',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'Fans',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'News',
-          href: '#',
-          current: false,
-        },
-        {
-          text: 'Shop',
-          href: '#',
-          current: false,
-        },
-    
-      ]
+    return {
+      menu
     }
   }
 }
